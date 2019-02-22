8a4378ebbf0cb6787525f2351083f1e3b220e978
Derek Norman <dereknorman42@gmail.com>
2019-02-22 12:41:34 -0700

    text changes

---
+++ js/userdatabase.js
@@ -0,0 +1,12 @@
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//     host:"localhost",
+//     user: "dereknorman42@gmail.com",
+//     password: "Derekmnmn42"
+// });
+
+// con.connect(function(err) {
+//     if(err) throw err;
+//     console.log("Connected!");
+// });
